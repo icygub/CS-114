e9f5c6295bc876d363c172599bcec58c99a97096
icygub <jchubbs95@gmail.com>
2016-10-14 17:35:22 -0600

    Add files via upload

--- Assignment5c.java
+++ Assignment5c.java
@@ -1,8 +1,17 @@
 package chapter4;
 import java.util.Scanner;
 
-public class Assignment5c {
+/**
+ * Prints snarks....
+ * Group 10: John and Abigail
+ * CS 114 Brother Masterson
+ */
 
+public class Assignment5c {
+    /**
+     * Program starts with user input
+     * @param args
+     */
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("How many snarks to start with? ");
@@ -10,18 +19,30 @@ public class Assignment5c {
         snark(startingSnarks);
     }
 
+    /**
+     * Adds one to numSnarks
+     * @param numSnarks
+     */
     public static void snark(int numSnarks) {
         numSnarks += 1;
         System.out.println(numSnarks);
         moreSnarks(numSnarks);
     }
 
+    /**
+     * Adds ten to multiSnarks
+     * @param multiSnarks
+     */
     public static void moreSnarks(int multiSnarks) {
         multiSnarks += 10;
         System.out.println(multiSnarks);
         powerSnarks(multiSnarks);
     }
 
+    /**
+     * lotsOfSharks to the power of five
+     * @param lotsOfSnarks
+     */
     public static void powerSnarks(int lotsOfSnarks) {
         lotsOfSnarks = (int) Math.pow(lotsOfSnarks, 5);
         System.out.println(lotsOfSnarks);
