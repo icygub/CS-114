3c573e9fa43e04232e1f987316c0a0d1e327c8a4
icygub <jchubbs95@gmail.com>
2016-11-10 15:49:17 -0700

    114 9c

--- Main.java
+++ Main.java
@@ -1,12 +1,61 @@
-package piggy.pack;
+package chapter8;
+import java.util.Scanner;
+import java.util.Arrays;
 
-/**
- * Created by asus on 10/1/2016.
- */
 public class Main {
+    /**
+     * User input is placed in inputList[], then gets passed calcDups(inputList)
+     * @param args
+     */
+    public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+        int input[] = new int[10];
+        int amount = 0;
+        System.out.println("Enter integers between 1 and 100. Hit enter to finish:");
+        for(int i = 0; i<input.length; i++){
+            int tester = sc.nextInt();
+            if (tester == 0){
+                break;
+            }
+            else{
+                input[i] = tester;
+            }
+            amount++;
+        }
+        int inputList[] = Arrays.copyOf(input, amount);
 
-    public static void main (String[] args){
-        Pig game = new Pig();
-        game.theGame();
+        calcDups(inputList);
+    }
+
+    public static void calcDups(int inputList[]) {
+        int dups[] = new int[inputList.length];
+        int dupsInInputList = 0;
+        int index = 0;
+
+        for(int i : inputList){
+            if (isInDups(dups, i)) {
+                continue;
+            }
+            else{
+                for(int secI : inputList){
+                    if(i == secI){
+                        dupsInInputList++;
+                    }
+                }
+                dups[index] = i;
+                index++;
+                System.out.println(i + " appears " + dupsInInputList + " times");
+            }
+            dupsInInputList = 0;
+        }
+    }
+
+    public static boolean isInDups(int list[], int value){
+        for(int i : list){
+            if(i == value){
+                return true;
+            }
+        }
+        return false;
     }
 }
